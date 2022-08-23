34661650cc0d690cae131afbd4d00333b6dabc80
Dasol Jessi Lee <dasol.jlee@gmail.com>
2022-08-23 11:25:54 -0700

    adjusted js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,36 @@ $("#currentDay").html(todayDate);
 
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
+        var text = $(this).child(".description").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
+        $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+        console.log("Appointment Added to localstorage");
     })
    
     function timeTracker() {
-        var timeNow = moment().hour();
+        var present = moment().hour();
 
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            if (blockTime < timeNow) {
+            if (blockTime < present) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            else if (blockTime === timeNow) {
+            else if (blockTime === present) {
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
@@ -45,6 +57,7 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
     timeTracker();
 })
