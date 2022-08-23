9eca108de84d32356488771ee732fa506b3bc635
Dasol Jessi Lee <dasol.jlee@gmail.com>
2022-08-23 11:29:13 -0700

    added style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,6 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
-        $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    $("#hour18 .description").val(localStorage.getItem("hour18"));
         console.log("Appointment Added to localstorage");
     })
    
